99130c234e5b534b41ee34900145e90c7224ecad
Fernando VR <fernando.2020.pp@outlook.com>
2022-10-18 22:02:43 +0200

    feat: add history endpoint to show history of operations

--- src/main/java/com/kata/bank/OperationController.java
+++ src/main/java/com/kata/bank/OperationController.java
@@ -4,10 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -43,5 +40,10 @@ public class OperationController {
             "{ \"responseMessage\": \"Success\", \"amount\": \"" + operation.getAmount() + "\"}",
             HttpStatus.OK
     );
+  }
+
+  @GetMapping(value = "/history", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> history() {
+    return new ResponseEntity<>("{ \"responseMessage\": \"Success\"}", HttpStatus.OK);
   }
 }
