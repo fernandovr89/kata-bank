23934044568a211f54681ec5efccbb3097d0f649
Fernando VR <fernando.2020.pp@outlook.com>
2022-10-18 20:16:51 +0200

    feat: add deposit endpoint in controller

---
+++ src/main/java/com/kata/bank/OperationController.java
@@ -0,0 +1,24 @@
+package com.kata.bank;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OperationController {
+
+  @PostMapping(
+    value = "/deposit",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<?> deposit(@RequestBody String body) {
+    return new ResponseEntity<>("{ \"responseMessage\": \"Success\"}", HttpStatus.OK);
+  }
+}
