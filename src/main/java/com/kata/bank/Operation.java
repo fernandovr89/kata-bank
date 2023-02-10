16b51881b96bae1daee214d0be641ee8fb64c647
Fernando VR <fernando.2020.pp@outlook.com>
2023-02-10 22:13:41 +0100

    style: write by prettier

--- src/main/java/com/kata/bank/Operation.java
+++ src/main/java/com/kata/bank/Operation.java
@@ -1,12 +1,11 @@
 package com.kata.bank;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -16,6 +15,7 @@ public class Operation {
   @NotNull(message = "The field amount is mandatory")
   @Positive(message = "The operation amount must be greater than zero")
   private Double amount;
+
   private LocalDateTime date;
   private Double balance;
   private OperationType operationType;
